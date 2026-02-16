f761ff7afd27c68fc841b77b5c3c31f9fb69053e
Maddy <magandteal@gmail.com>
2026-02-16 04:39:30 -0500

    small bug fix for commited blocks

--- SprintZero/obj/Debug/net9.0/SprintZero.AssemblyInfo.cs
+++ SprintZero/obj/Debug/net9.0/SprintZero.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+158d188d166728db55d30edb3396135213a0ba63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+583ae72bafedb112dc9aafc41d277e4ff21b45d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
