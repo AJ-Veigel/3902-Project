7b26eda5ce9b247aded230bed6aae5398181cbe1
AJ Veigel <veigel.7@osu.edu>
2026-02-16 20:40:27 -0500

    Add files via upload

---
+++ IMario.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpriteZero.Marios
+{
+    public interface IMario
+    {
+        
+        Vector2 position {get;set;}
+        Boolean Sprint {get;set;}
+        Boolean Swim {get;set;}
+        Boolean Crouch {get;set;}
+        void MoveRight();
+        void MoveLeft();
+        void Jump();
+        void Fireball();
+
+        
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch spriteBatch);
+    }
+}
