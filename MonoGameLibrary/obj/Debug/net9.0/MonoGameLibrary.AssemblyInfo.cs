37e722870409a5b2665b260c0957bf44348d7d95
Adam Novak <novak.388@buckeyemail.osu.edu>
2026-02-23 15:16:05 -0500

    fix conflicts again

--- MonoGameLibrary/obj/Debug/net9.0/MonoGameLibrary.AssemblyInfo.cs
+++ MonoGameLibrary/obj/Debug/net9.0/MonoGameLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf5e8475da4fb8783b4b28f96037378e1b61ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fabaa71252518dd7e01190ca26f7947f1a963d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MonoGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SprintZero/obj/Debug/net9.0/SprintZero.AssemblyInfo.cs
+++ SprintZero/obj/Debug/net9.0/SprintZero.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cf5e8475da4fb8783b4b28f96037378e1b61ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fabaa71252518dd7e01190ca26f7947f1a963d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
