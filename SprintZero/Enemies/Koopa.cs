e628b434b6d7356dfac8d65121092ec91cb8ae49
ThePizza1k <44148753+ThePizza1k@users.noreply.github.com>
2026-02-23 13:06:28 -0500

    Koopa drawing (kinda)
    i implemented the Draw method but it's not actually being used yet

--- SprintZero/Enemies/Koopa.cs
+++ SprintZero/Enemies/Koopa.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGameLibrary;
 using MonoGameLibrary.Graphics;
 using SpriteZero.Enemies;
 
 public class Koopa : IEnemy
 {
-	private const float WALK_TIME = 0.25f;
+	private static TextureRegion[] green;
+	private static TextureRegion[] red;
+	private static TextureRegion[] blue;
+
+
+    private const float WALK_TIME = 0.25f;
 	private const float CLOSE_AWAKEN_TIME = 3.0f;
 	private const float AWAKEN_TIME = 5.0f;
 	public enum KoopaType { Green, Red, Blue };
 	public Vector2 position { get; set; }
 	public bool Dead { get; set; }
 	private bool FacingLeft { get; set; }
-
-	private enum KoopaStates { Walk1, Walk2, ShellStill, ShellStill2, ShellMoving}
+	private KoopaType Type { get; set; }
+	private enum KoopaStates { Walk1, Walk2, ShellStill, ShellStill2, ShellMoving }
 	private KoopaStates KoopaState { get; set; }
 	private float KoopaTimer { get; set; }
-
-	private bool WillWalkOff { get; set; }
 
 	private readonly TextureRegion walk1;
 	private readonly TextureRegion walk2;
@@ -29,45 +34,53 @@ public class Koopa : IEnemy
 		blue: (453, 875, 16, 24); (483, 874, 16, 24);
 	*/
 
+	public static void loadTextures(ContentManager content)
+	{
+		TextureAtlas atlas = TextureAtlas.FromFile(content, "images/koopadefinition.xml");
+		green[(int)KoopaStates.Walk1] = atlas.GetRegion("greenWalk1");
+		green[(int)KoopaStates.Walk2] = atlas.GetRegion("greenWalk2");
+		green[(int)KoopaStates.ShellStill] = atlas.GetRegion("greenShell1");
+		green[(int)KoopaStates.ShellStill2] = atlas.GetRegion("greenShell2");
+		green[(int)KoopaStates.ShellMoving] = atlas.GetRegion("greenShell1");
+        red[(int)KoopaStates.Walk1] = atlas.GetRegion("redWalk1");
+        red[(int)KoopaStates.Walk2] = atlas.GetRegion("redWalk2");
+        red[(int)KoopaStates.ShellStill] = atlas.GetRegion("redShell1");
+        red[(int)KoopaStates.ShellStill2] = atlas.GetRegion("redShell2");
+        red[(int)KoopaStates.ShellMoving] = atlas.GetRegion("redShell1");
+        blue[(int)KoopaStates.Walk1] = atlas.GetRegion("blueWalk1");
+        blue[(int)KoopaStates.Walk2] = atlas.GetRegion("blueWalk2");
+        blue[(int)KoopaStates.ShellStill] = atlas.GetRegion("blueShell1");
+        blue[(int)KoopaStates.ShellStill2] = atlas.GetRegion("blueShell2");
+        blue[(int)KoopaStates.ShellMoving] = atlas.GetRegion("blueShell1");
+    }
+
 	public Koopa(KoopaType type)
 	{
 		Dead = false;
 		position = new Vector2(0.0f, 0.0f);
 		FacingLeft = true;
-		WillWalkOff = true;
 		KoopaState = KoopaStates.Walk1;
 		KoopaTimer = WALK_TIME;
-		switch (type)
-		{
-			case KoopaType.Green:
-				walk1 = new TextureRegion(); // We really need a content handler.
-				walk2 = new TextureRegion();
-				break;
-			case KoopaType.Red:
-				walk1 = new TextureRegion();
-				walk2 = new TextureRegion();
-				WillWalkOff = false;
-				break;
-			case KoopaType.Blue:
-				walk1 = new TextureRegion();
-				walk2 = new TextureRegion();
-				break;
-		}
+		Type = type;
 	}
 
 	public void Draw(SpriteBatch spriteBatch)
 	{
 		float scaleX = FacingLeft ? 1.0f : -1.0f; // Normal scale if facing left, otherwise negative scale for facing right.
 		int offX = FacingLeft ? 0 : -16; // I suspect this is needed but idk for sure.
-		switch (this.KoopaState)
+		TextureRegion[] sprites = null;
+		if (Type == KoopaType.Green)
 		{
-			case KoopaStates.Walk1:
-				break;
-			case KoopaStates.Walk2:
-				break;
-			default:
-				throw new NotImplementedException();
+			sprites = green;
+		} else if (Type == KoopaType.Red)
+		{
+			sprites = red;
+		} else
+		{
+			sprites = blue;
 		}
+		TextureRegion texture = sprites[(int)KoopaState];
+		texture.Draw(spriteBatch, new Vector2(position.X + offX, position.Y), Color.White, 0.0f, new Vector2(0, 0), new Vector2(scaleX, 1.0f), SpriteEffects.None, 0.0f);
 	}
 
 	private void HandleTimer()
@@ -76,7 +89,7 @@ public class Koopa : IEnemy
         {
             switch (this.KoopaState)
             {
-                case KoopaStates.Walk1:
+                case KoopaStates.Walk1: // These animate the walking.
                     this.KoopaState = KoopaStates.Walk2;
                     this.KoopaTimer += WALK_TIME;
                     break;
