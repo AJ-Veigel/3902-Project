82bcf67a10af0e605e7ab3b069ee9c3f54199918
ThePizza1k <44148753+ThePizza1k@users.noreply.github.com>
2026-03-11 12:44:08 -0400

    Make side detection actually decent

--- SprintZero/Hitbox.cs
+++ SprintZero/Hitbox.cs
@@ -19,29 +19,41 @@ namespace SprintZero
             this.x = x; this.y = y; this.width = width; this.height = height;
         }
 
+        private static float MinimumPositive(params float[] vals)
+        {
+            float min = float.PositiveInfinity;
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if (vals[i] < min && vals[i] >= 0.0f)
+                {
+                    min = vals[i];
+                }
+            }
+            return min;
+        }
 
         // Returns the side of the second hitbox that the first hitbox collides with, or CollisionSide.None if no collision.
         public CollisionSide CollidesWith(Vector2 selfOffset, Hitbox other, Vector2 otherOffset)
         {
             Vector2 diff = otherOffset - (selfOffset + new Vector2(this.x, this.y)); // How much offset from this hitbox the other hitbox is.
             // This X,Y is 0.
-            var otherX = other.x + diff.X; var otherY = other.y + diff.Y;
+            float otherX = other.x + diff.X; float otherY = other.y + diff.Y;
             if (    otherX > this.width
                 ||  otherY > this.height
                 || (otherX + other.width) < 0.0f
                 || (otherY + other.height) < 0.0f
             ) { return CollisionSide.None;} // 
 
-            var left = this.width - otherX; // How 'far' this collides with the left side of other
-            var top = this.height - otherY; // How 'far' this collides with the top side of other u get the point.
-            var right = otherX + other.width;
-            var bottom = otherY + other.height;
+            float left = this.width - otherX; // How 'far' this collides with the left side of other
+            float top = this.height - otherY; // How 'far' this collides with the top side of other u get the point.
+            float right = otherX + other.width;
+            float bottom = otherY + other.height;
 
-            var max = (new float[] { left, top, right, bottom }).Max(); // Evil?
+            float real = MinimumPositive(left, top, right, bottom);
 
-            if (max == left) { return CollisionSide.Left; }
-            else if (max == top) { return CollisionSide.Top; }
-            else if (max == right) { return CollisionSide.Right; }
+            if (real == left) { return CollisionSide.Left; }
+            else if (real == top) { return CollisionSide.Top; }
+            else if (real == right) { return CollisionSide.Right; }
             return CollisionSide.Bottom;
         }
 
