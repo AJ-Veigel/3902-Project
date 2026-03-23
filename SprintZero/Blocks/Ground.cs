2dcd1e6cfd624df07837afca4a92f1a1490bcd90
AJ Veigel <veigel.7@osu.edu>
2026-03-23 13:10:58 -0400

    Added proper gravity to Mario

--- SprintZero/Blocks/Ground.cs
+++ SprintZero/Blocks/Ground.cs
@@ -6,44 +6,45 @@ using SprintZero.Marios;
 
 public class Ground : IBlock
 {
- 
+
     private TextureRegion sprite;
-    public Vector2 location {get;set;}
-    public Rectangle Collider {get; set;}
+    public Vector2 location { get; set; }
+    public Rectangle Collider { get; set; }
     private const float SCALE = 4f;
     public Ground(TextureRegion region)
     {
         sprite = region;
         location = new Vector2(600, 600);
-        Collider = new Rectangle((int)location.X, (int)location.Y, (int)sprite.Width, (int)sprite.Height);
+        Collider = new Rectangle((int)location.X, (int)location.Y, (int)(sprite.Width * SCALE), (int)(sprite.Height * SCALE));
     }
     public void Update(GameTime gameTime)
-{
-    Collider = new Rectangle(
-        (int)location.X,
-        (int)location.Y,
-        (int)(sprite.Width * SCALE),
-        (int)(sprite.Height * SCALE)
-    );
-}
+    {
+        Collider = new Rectangle(
+            (int)location.X,
+            (int)location.Y,
+            (int)(sprite.Width * SCALE),
+            (int)(sprite.Height * SCALE)
+        );
+    }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        sprite.Draw(spriteBatch,location,Color.White,0f,Vector2.One,4f,SpriteEffects.None,0f);
+        sprite.Draw(spriteBatch, location, Color.White, 0f, Vector2.One, 4f, SpriteEffects.None, 0f);
 
     }
-public void onCollision(IMario mario, CollisionSide theSide)
-{
-    if (theSide == CollisionSide.Top)
+    public void onCollision(IMario mario, CollisionSide theSide)
     {
-        mario.location = new Vector2(
-            mario.location.X,
-            Collider.Top - mario.MarioCollider.Height
-        );
+        if (theSide == CollisionSide.Top)
+        {
+            mario.location = new Vector2(
+                mario.location.X,
+                Collider.Top - mario.MarioCollider.Height
+            );
 
-        mario.Falling = false;
-        mario.Jumping = false;
-        mario.jumpStartHeight = mario.location.Y;
+            mario.Falling = false;
+            mario.Jumping = false;
+            mario.isOnGround = true;
+            mario.jumpStartHeight = mario.location.Y;
+        }
     }
-}
 }
