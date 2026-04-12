0e70544e517b7326ffa7426893640364ab8f6b50
Maddy <magandteal@gmail.com>
2026-04-12 05:29:22 -0400

    Addded pause and updated readme

--- SprintZero/Controllers/GameController.cs
+++ SprintZero/Controllers/GameController.cs
@@ -5,10 +5,10 @@ using SprintZero;
 
 public class GameController
 {
-    private SprintZero.Game1 game;
+    private Game1 game;
     private KeyboardState previousState, currentState;
 
-    public GameController(KeyboardState prev, KeyboardState curr, SprintZero.Game1 game)
+    public GameController(KeyboardState prev, KeyboardState curr, Game1 game)
     {
         previousState = prev;
         currentState = curr;
@@ -17,27 +17,25 @@ public class GameController
 
     public void UpdateGame()
     {
-        // Exit
+       
         if (currentState.IsKeyDown(Keys.Escape) && previousState.IsKeyUp(Keys.Escape))
             game.Exit();
 
-        // Reset
         if (currentState.IsKeyDown(Keys.R) && previousState.IsKeyUp(Keys.R))
             game.Reset();
 
-        // Play music
+     
         if (currentState.IsKeyDown(Keys.N) && previousState.IsKeyUp(Keys.N))
             game.play();
 
-        // Pause
+
         if (currentState.IsKeyDown(Keys.D5) && previousState.IsKeyUp(Keys.D5))
             game.PauseGame();
 
-        // Unpause
         if (currentState.IsKeyDown(Keys.D6) && previousState.IsKeyUp(Keys.D6))
             game.UnpauseGame();
 
-        // Media pause (independent of game pause)
+       
         if (currentState.IsKeyDown(Keys.M) && previousState.IsKeyUp(Keys.M))
             MediaPlayer.Pause();
     }
--- SprintZero/obj/Debug/net10.0/SprintZero.AssemblyInfo.cs
+++ SprintZero/obj/Debug/net10.0/SprintZero.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a32494cb584936e66c85b92ac0c0a1c83fef08e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ae0c441c8bbc7c3a097d008149d49f67d9a62c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SprintZero")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
